c2b2788f3a0e5e582c07f19bcfd65de338397a1e
yutomorita416 <yutomorita416@gmail.com>
2021-05-07 12:48:09 +0000

    finish chapter6

---
+++ score.rb
@@ -0,0 +1,7 @@
+score = 90
+if (score >= 50 || score <= 100) && score >= 80
+  puts "得点は50点以上または100点以下で、かつ80点以上です"
+end
+if score >= 50 || (score <= 100 && score >= 80)
+  puts "得点は50点以上、または80点以上100点以下です。"
+end
